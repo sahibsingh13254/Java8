09b36c7add0e2980d4baec26d79146629ea7d91d
sahibsingh13254 <sahibs98765432@gmail.com>
2026-07-17 15:21:50 +0530

    Added AND,OR,Negate and passing objects to predecate

--- src/predicate/main.java
+++ src/predicate/main.java
@@ -29,5 +29,56 @@ public class main {
         // other example of string can be
         Predicate<String> checkLetterV = x -> x.toLowerCase().charAt(0) == 'v';
         System.out.println(checkLetterV.test("Vision"));
+
+        Predicate<String> endsWithLetterV = x -> x.toLowerCase().charAt(x.length()-1) == 'l';
+
+         // AND
+        Predicate<String> and  = checkLetterV.and(endsWithLetterV);
+        System.out.println(and.test("vipul"));
+
+        //OR
+        Predicate<String> or  = checkLetterV.or(endsWithLetterV);
+        System.out.println(or.test("bipin"));
+
+        //Negate
+        System.out.println(checkLetterV.negate().test("vipin"));
+
+
+        // passing objects to predecate
+        student sahib = new student(1 , "sahib");
+        student daksh = new student(2 , "daksh");
+        student anushk = new student(3, "anushk");
+
+        Predicate<student> studentPredicate = x -> x.getId() > 1;
+        System.out.println(studentPredicate.test(daksh));
+
     }
-}
+    private static class student{
+        int id;
+
+        public student(int id, String name) {
+            this.id = id;
+            this.name = name;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public int getId() {
+            return id;
+        }
+
+        public void setId(int id) {
+            this.id = id;
+        }
+
+        String name;
+
+        }
+    }
+
